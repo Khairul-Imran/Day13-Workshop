78b74de559d93185e166c326f14d67443883f9b1
Khairul <Khairulimran6810@gmail.com>
2023-11-26 18:57:30 +0800

    First Commit

---
+++ src/test/java/sg/edu/nus/iss/Day13Workshop/Day13WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Day13Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
